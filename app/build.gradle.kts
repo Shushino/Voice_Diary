00e46df25f12f743867b5a6c7cc405a7e41515df
Shushino <georgeomowu@gmail.com>
2026-05-18 17:55:35 +0100

    build: update compileSdk to 35 to satisfy dependency requirements while keeping targetSdk 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.george.voicediary"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
